fa1634ded4020c7faf43a736c93a22ddb382b3fe
Dominyk Tiller <dominyktiller@gmail.com>
2016-03-20 02:21:50 +0000

    jenkins-lts 1.642.3
    Closes #1190.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- jenkins-lts.rb
+++ jenkins-lts.rb
@@ -1,8 +1,8 @@
 class JenkinsLts < Formula
   desc "Extendable open-source CI server"
   homepage "http://jenkins-ci.org/#stable"
-  url "http://mirrors.jenkins-ci.org/war-stable/1.642.2/jenkins.war"
-  sha256 "024b04524f230dea62424de95f372d8c89ed942b21dbef68117b259842b96907"
+  url "http://mirrors.jenkins-ci.org/war-stable/1.642.3/jenkins.war"
+  sha256 "942478fbf16dfe8b338fd2402ed406a6d74f9154f749c424f3d2d1d389594d72"
 
   depends_on :java => "1.7+"
 

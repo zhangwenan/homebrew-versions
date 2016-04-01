d4f85722f524f2ff77140ba74a98aab06bfa984c
BrewTestBot <brew-test-bot@googlegroups.com>
2016-04-01 16:39:18 +0100

    rebar3: add 3.0.0 bottle.

--- rebar3.rb
+++ rebar3.rb
@@ -6,6 +6,13 @@ class Rebar3 < Formula
 
   head "https://github.com/rebar/rebar3.git"
 
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "c119a030cdbe880bb0e29b9b4bec586938566267f6c6c587bcbc5dbcc1d6dd74" => :el_capitan
+    sha256 "e97f4343b963f8f261886bafccbe83b818d8aaee7504a7489d61bf6a28be958b" => :yosemite
+    sha256 "dbefd00269a65c04f43053120c6b4865dc7cbc0c9cbf24add8076ec2ad5b12ca" => :mavericks
+  end
+
   depends_on "erlang"
 
   def install

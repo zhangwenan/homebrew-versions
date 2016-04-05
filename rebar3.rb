2d26d3f57d62cd35649c032a387ec4c0d7f10ad0
Rui Coelho <rui.coelho@miniclip.com>
2016-04-05 11:27:09 +0100

    rebar3 3.1.0
    Closes #1206.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- rebar3.rb
+++ rebar3.rb
@@ -1,8 +1,8 @@
 class Rebar3 < Formula
   desc "Erlang build tool"
   homepage "https://github.com/erlang/rebar3"
-  url "https://github.com/erlang/rebar3/archive/3.0.0.tar.gz"
-  sha256 "886acd7bb7cfb99d105d9f900b93f8cc6e571330838004a75b6bac2a4ac1baca"
+  url "https://github.com/erlang/rebar3/archive/3.1.0.tar.gz"
+  sha256 "b426cf7829d5df0d6d3e50cd501a1688bdbc878b0ca69d63240a0614afbd9c64"
 
   head "https://github.com/rebar/rebar3.git"
 

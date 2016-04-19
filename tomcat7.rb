48dbb63efa87f38b8ba64f489493db33bd8b56cc
Alan Orth <alanorth@users.noreply.github.com>
2016-04-19 11:52:23 +0300

    tomcat7 7.0.69 (#1226)

--- tomcat7.rb
+++ tomcat7.rb
@@ -1,8 +1,8 @@
 class Tomcat7 < Formula
   desc "Implementation of Java Servlet and JavaServer Pages"
   homepage "https://tomcat.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68.tar.gz"
-  sha256 "2c1a02422f265607de733521e54f18613e9dd5efc2861914fe45a858227bb8e0"
+  url "https://www.apache.org/dyn/closer.cgi?path=tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69.tar.gz"
+  sha256 "5202aab315355d001c41cfe81820e55d3dcd4b4da191054464c68182fa2a9c0f"
 
   bottle :unneeded
 
@@ -11,9 +11,9 @@ class Tomcat7 < Formula
   option "with-fulldocs", "Install full documentation locally"
 
   resource "fulldocs" do
-    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.68/bin/apache-tomcat-7.0.68-fulldocs.tar.gz"
-    version "7.0.68"
-    sha256 "c601d81217120ff2b645528ec579e33d1e5dc08cc27a7364aa5fad725b9b5f08"
+    url "https://www.apache.org/dyn/closer.cgi?path=/tomcat/tomcat-7/v7.0.69/bin/apache-tomcat-7.0.69-fulldocs.tar.gz"
+    version "7.0.69"
+    sha256 "1890a03ca2b9169b7f6ac206b0f19e06cd9a5416e4f30b285c957ced7fd80343"
   end
 
   # Keep log folders

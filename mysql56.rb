554f653ac01efa52a96c11496780585448847ef1
Björn Blomqvist <bjorn.blomqvist@bukowskis.se>
2016-06-08 16:06:07 +0200

    mysql56 5.6.31
    Changed source url to point to latest patch release. The current url gets 404.
    
    Closes #1284.
    
    Signed-off-by: Dominyk Tiller <dominyktiller@gmail.com>

--- mysql56.rb
+++ mysql56.rb
@@ -1,8 +1,8 @@
 class Mysql56 < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/5.6/en/"
-  url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.29.tar.gz"
-  sha256 "6ac85b75b2dfa8c232725dda25469df37bf4e48b408cc0978d0dfc34c25a817f"
+  url "https://cdn.mysql.com/Downloads/MySQL-5.6/mysql-5.6.31.tar.gz"
+  sha256 "6df1389bbf899025aee6be0f4a12b8b0135e6de7db83e3ea20201ad3633ba424"
 
   bottle do
     sha256 "e2527ad593b76c81507f580567ba79055c1f6cf5329deeb74babceb9ea850e21" => :el_capitan

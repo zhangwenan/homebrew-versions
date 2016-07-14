57ee1612893db30681707180229e9efecfbef1d4
Alexander Altman <alexanderaltman@me.com>
2016-07-13 20:31:40 -0700

    bash-completion2 2.3
    bash-completion2 (2.3)
    
    The development location has changed; the old homepage now redirects to the new one.
    
    Closes #1312.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- bash-completion2.rb
+++ bash-completion2.rb
@@ -1,10 +1,9 @@
 class BashCompletion2 < Formula
   desc "Programmable completion for Bash 4.0+"
-  homepage "https://bash-completion.alioth.debian.org/"
-  url "https://mirrors.kernel.org/debian/pool/main/b/bash-completion/bash-completion_2.1.orig.tar.bz2"
-  mirror "https://mirrors.ocf.berkeley.edu/debian/pool/main/b/bash-completion/bash-completion_2.1.orig.tar.bz2"
-  sha256 "2b606804a7d5f823380a882e0f7b6c8a37b0e768e72c3d4107c51fbe8a46ae4f"
-  revision 2
+  homepage "https://github.com/scop/bash-completion"
+  url "https://github.com/scop/bash-completion/releases/download/2.3/bash-completion-2.3.tar.xz"
+  sha256 "b2e081af317f3da4fff3a332bfdbebeb5514ebc6c2d2a9cf781180acab15e8e9"
+  head "https://github.com/scop/bash-completion.git"
 
   bottle do
     cellar :any_skip_relocation
@@ -16,34 +15,11 @@ class BashCompletion2 < Formula
 
   conflicts_with "bash-completion"
 
-  # All three fix issues with GNU extended regexs
-  patch do
-    url "https://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=patch;h=f230cfddbd12b8c777040e33bac1174c0e2898af"
-    sha256 "b557b2f71a1376b51bf2de1c56f181b27111381cb3cac727144d65d94ab1758a"
-  end
-
-  patch do
-    url "https://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=patch;h=3ac523f57e8d26e0943dfb2fd22f4a8879741c60"
-    sha256 "b680b347d8f1330cbae47b76ec6d9e9ec15459a7c89c2c767855e47afbebed96"
-  end
-
-  patch do
-    url "https://anonscm.debian.org/gitweb/?p=bash-completion/bash-completion.git;a=patch;h=50ae57927365a16c830899cc1714be73237bdcb2"
-    sha256 "7a5dda29cb0c0ba4fc747fd2163c8041efe4b157b71708b4e9db5a0048588e6b"
-  end
-
   # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=739835
   # resolves issue with completion of files/directories with spaces in the name.
   patch do
     url "https://anonscm.debian.org/cgit/bash-completion/debian.git/plain/debian/patches/00-fix_quote_readline_by_ref.patch?id=d734ca3bd73ae49b8f452802fb8fb65a440ab07a"
     sha256 "7304f8fb4ad869f1b3d6f3456b2750246ddedef6fc307939bf403bf528f2fdf1"
-  end
-
-  # https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=739835
-  # https://bugs.launchpad.net/ubuntu/+source/bash-completion/+bug/1289597
-  patch do
-    url "https://raw.githubusercontent.com/Homebrew/formula-patches/9d38db2889e3af2deba8edabca368e4e02d0f73e/bash-completion2/bug-739835.patch"
-    sha256 "126e535c2da21f67ec54522c3deaf210921c3ee89a863fbb585e7dde39d67aeb"
   end
 
   def install
@@ -60,5 +36,9 @@ class BashCompletion2 < Formula
         . $(brew --prefix)/share/bash-completion/bash_completion
       fi
     EOS
+  end
+
+  test do
+    system "test", "-f", "#{share}/bash-completion/bash_completion"
   end
 end

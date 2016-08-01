9870dc794f6765c3cf6910d888c33431e5ee4b21
tuxtof <tuxtof@geo6.net>
2016-08-02 00:20:16 +0200

    docker111 1.11.2 (new formula)
    Closes #1328.
    
    Signed-off-by: Mike McQuaid <mike@mikemcquaid.com>

--- docker111.rb
+++ docker111.rb
@@ -1,18 +1,11 @@
-class Docker110 < Formula
+class Docker111 < Formula
   desc "Pack, ship and run any application as a lightweight container"
   homepage "https://www.docker.com/"
   url "https://github.com/docker/docker.git",
-      :tag => "v1.10.3",
-      :revision => "20f81dde9bd97c86b2d0e33bbbf1388018611929"
+      :tag => "v1.11.2",
+      :revision => "b9f10c951893f9a00865890a5232e85d770c1087"
 
   head "https://github.com/docker/docker.git"
-
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "8a7d140e71f3f4b0f41849dc765f1e32b83622ebb7132e934a2368311b97f7ec" => :el_capitan
-    sha256 "eab150519ddfe8a3e078f41460b62591a5edd80e74ead9929624dd074f5d9e41" => :yosemite
-    sha256 "4ab7b3974aa60b242c0424e7e79b50f9d8997d2b86b5eecd747a84594483db15" => :mavericks
-  end
 
   option "with-experimental", "Enable experimental features"
   option "without-completions", "Disable bash/zsh completions"
